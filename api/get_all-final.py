c75ea4f924bb9e7218f584578e7f4d03b47cc06f
Triwoods2333 <90879899+Triwoods2333@users.noreply.github.com>
2024-04-16 12:59:13 +0800

    Update get_all-final.py

--- api/get_all-final.py
+++ api/get_all-final.py
@@ -1,18 +1,30 @@
 import requests
+import sys
+import time
+sys.setrecursionlimit(1000000)
 
-
+f = open(f'result_final.txt', 'ab')
+error_index = 0
 def get_latest_changes():
-    index = 0
+    global f,error_index
+    print("starting")
+    now_index = 0
     try:
-        f = open(f'result_final.txt', 'ab')
         response = requests.get('https://skimdb.npmjs.com/registry/_all_docs', stream=True, timeout=10)
         response.raise_for_status()
         for chunk in response.iter_content(chunk_size=8192):
-            index += 1
-            if index >= 0:
+            now_index += 1
+            print(error_index)
+            print(now_index)
+            if now_index >= error_index:
                 f.write(chunk)
 
     except Exception:
-        print('failed index:' + str(index))
+        if now_index >= error_index:
+            error_index = now_index
+        print('failed index:' + str(error_index))
+        print('restart...')
+        time.sleep(10)
+        get_latest_changes()
 
 get_latest_changes()
